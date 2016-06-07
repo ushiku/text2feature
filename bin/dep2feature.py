c2d4b0b958fb28b7213c11385e83e31d777af90a
ushiku <d460655046504@gmail.com>
2016-06-07 13:43:05 +0900

    ファイルを繋げてよむ

--- bin/dep2feature.py
+++ bin/dep2feature.py
@@ -47,22 +47,17 @@ def ReturnBIGGEST(n, array):
     for vector2 in array.todense():
         vector2 = np.squeeze(np.asarray(vector2))
         sim_vector.append(1-cosine(vector1, vector2))  # ここcosineが1-cosine距離で定式している?
-        
     sim_vector[n] = -1  # 自分自身は無視
-    print("original:",text_word[n])
+    print("original:", corpus_list[1][n])
     for a in range(0, number):  # 上位n個を出す(n未満の配列には対応しないので注意)
-        print("simirality:", np.nanmax(sim_vector), "answer ", a, ":", text_word[np.nanargmax(sim_vector)])
+        print("simirality:", np.nanmax(sim_vector), "answer ", a, ":", corpus_list[1][np.nanargmax(sim_vector)])
         sim_vector[np.nanargmax(sim_vector)] = -1
     print()
     return 0    
 
 def caluculate(vectorizer, text):
     X = vectorizer.fit_transform(text)
-    #print(X.toarray())
-    #print('feature一覧:', vectorizer.get_feature_names())
-#    print(X)
-    ReturnBIGGEST(0, X)
-#    ReturnBIGGEST(1, X)
+    ReturnBIGGEST(0, X)  # 0がinput_text
     return 0
 
 def doc2vec_sim(input_text, text_word_for_doc2vec):  # doc2vecのモデルを使って類似度を計算。未知語はないものとして
@@ -73,61 +68,73 @@ def doc2vec_sim(input_text, text_word_for_doc2vec):  # doc2vecのモデルを使
     for words_for_doc2vec in text_word_for_doc2vec:
         filtered_words = list( set(words_for_doc2vec) & set(model.vocab.keys()) )
         filtered_sim = model.n_similarity(input_text, filtered_words)
+#        print(input_text, filtered_words, "|sim=:", filtered_sim)
         if type(filtered_sim) == numpy.ndarray: #  なぜか、simがどちらかが空（未知語しかない）ときにndarrayを返すため
             filtered_sim = 0
         sim_vector.append(filtered_sim)
-    print("original:", text_word[0])
+    print("original:", input_list[1][0])
     for a in range(0, number):  # 上位n個を出す(n未満の配列には対応しないので注意)
-        print("simirality:", np.nanmax(sim_vector), "answer ", a, ":", text_word[np.nanargmax(sim_vector)])
+        print("simirality:", np.nanmax(sim_vector), "answer ", a, ":", corpus_list[1][np.nanargmax(sim_vector)])
         sim_vector[np.nanargmax(sim_vector)] = -1
     print()
     return 0
 
-    return 0
-    
-number = 5  # 上位何個を出すか?
-text_full = []  # かかり受け情報を含んだそのままのEDAの結果をlist化
-text_word = []  # scikit-learn ように ['This is a pen', 'That is a pen']というlist
-text_word_for_doc2vec = []  # doc2vec ように[['this', 'is'], [..]]
-fulls = []
-words = ''
-words_for_doc2vec = []
-input_text = '../../../text2feature/corpus/sample.eda'
-corpus = '../../../text2feature/corpus/full.eda'
-data = [input_text, corpus]
-for line in fileinput.input(data):
-    line = line.strip()
-    if re.match('ID', line):
-        continue
-    if line == '':
-        text_full.append(fulls)
-        fulls = []
-        text_word.append(words.strip())
-        text_word_for_doc2vec.append(words_for_doc2vec)
-        words = ''
-        words_for_doc2vec = []
-        continue
-    units = line.split(' ')
-    fulls.append(line)
-    words = words + ' ' +  units[2]
-    words_for_doc2vec.append(units[2])
-text_full.append(fulls)
-text_word.append(words.strip())
-text_word_for_doc2vec.append(words_for_doc2vec)
-text_word_dep = [] 
-text_word_dep_uni = []
-for text in text_full:
-    depbigram, depbigram_unigram = text2dip(text)
-    text_word_dep.append(depbigram.strip())
-    text_word_dep_uni.append(depbigram_unigram.strip())
 
+def eda2list(file_eda):
+    '''
+    edaファイルを展開して、listを吐く
+    '''
+    text_full = []  # かかり受け情報を含んだそのままのEDAの結果をlist化
+    text_word = []  # scikit-learn ように ['This is a pen', 'That is a pen']というlist
+    text_word_for_doc2vec = []  # doc2vec ように[['this', 'is'], [..]]
+    fulls = []
+    words = ''
+    words_for_doc2vec = []
+    for line in open(file_eda, 'r'):
+        line = line.strip()
+        if re.match('ID', line):
+            continue
+        if line == '':
+            text_full.append(fulls)
+            fulls = []
+            text_word.append(words.strip())
+            text_word_for_doc2vec.append(words_for_doc2vec)
+            words = ''
+            words_for_doc2vec = []
+            continue
+        units = line.split(' ')
+        fulls.append(line)
+        words = words + ' ' +  units[2]
+        words_for_doc2vec.append(units[2])
+    text_full.append(fulls)
+    text_word.append(words.strip())
+    text_word_for_doc2vec.append(words_for_doc2vec)
+    text_word_dep = []
+    text_word_dep_uni = []
+    for text in text_full:
+        depbigram, depbigram_unigram = text2dip(text)
+        text_word_dep.append(depbigram.strip())
+        text_word_dep_uni.append(depbigram_unigram.strip())
+    return [text_full, text_word, text_word_for_doc2vec, text_word_dep, text_word_dep_uni]
+
+
+number = 5  # 上位何個を出すか?
+
+input_text = '../corpus/sample.eda'
+corpus = '../corpus/full.eda'
+data = [input_text, corpus]
+corpus_list = eda2list(corpus)
+input_list = eda2list(input_text)
+for n in range(0, 5):
+    input_list[n].extend(corpus_list[n])
+    corpus_list[n] = input_list[n]
 print('tfidf')
-caluculate(TfidfVectorizer(), text_word)  # tfidfモデル
+caluculate(TfidfVectorizer(), corpus_list[1])  # tfidfモデル
 print('dep')
-caluculate(CountVectorizer(), text_word_dep)  # depモデル
+caluculate(CountVectorizer(), corpus_list[3])  # depモデル
 print('depbigram-tfidf')
-caluculate(TfidfVectorizer(), text_word_dep)  # depbigram-tfidfモデル
+caluculate(TfidfVectorizer(), corpus_list[3])  # depbigram-tfidfモデル
 print('depbigram-unigram')
-caluculate(TfidfVectorizer(), text_word_dep_uni)  # depbigram-unigramモデル
+caluculate(TfidfVectorizer(), corpus_list[4])  # depbigram-unigramモデル
 print('doc2vec')
-doc2vec_sim(text_word_for_doc2vec[0], text_word_for_doc2vec)  # doc2vec
+doc2vec_sim(input_list[2][0], corpus_list[2])  # doc2vec
