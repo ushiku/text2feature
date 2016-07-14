445e075a406bd114da371b3f51f66d50c8116ce5
Atsushi Ushiku Local <atsushi@mgs06.(none)>
2016-07-14 15:00:22 +0900

    サンプル書き換え

--- bin/sample.py
+++ bin/sample.py
@@ -1,11 +1,15 @@
 from module.doc2vec import Doc2Vec
 from module.dep2feature import Dep2Feature
 import module.text2dep as text2dep
+import sys
 import pickle
 import pdb
 #テキストファイル(hoge.txt)とedaの学習モデルを引数として渡す
-#input_eda = text2dep.text2dep('../corpus/hoge.txt',  kytea_model='../model/model.bin', eda_model='../model/bccwj-20140727.etm')
-#input_eda = text2dep.text2dep('../corpus/hoge.txt',  kytea_model='../model/model.bin', eda_model='../model/bccwj-20140727.etm')
+
+
+#input_list = ['../corpus/input/1.txt', '../corpus/input/2.txt']
+#input_eda, input_eda_raw = text2dep.text2dep(input_list,  kytea_model='../model/model.bin', eda_model='../model/bccwj-20140727.etm')
+#print(input_eda)
 #f = open("../model/eda.dump", "wb")
 #pickle.dump(input_eda, f)            # ファイルに保存
 #f.close()
@@ -15,26 +19,29 @@ f = open('../model/eda.dump', 'rb')
 input_eda = pickle.load(f)
 f.close()
 
-#input_text = open('../corpus/sample.eda')
-# TODO: Tf:idf:
+#corpus_list = sys.argv[1:]
+#print(corpus_list)
+#corpus_eda, corpus_eda_raw = text2dep.text2dep(corpus_list,  kytea_model='../model/model.bin', eda_model='../model/bccwj-20140727.etm')
+#f = open("../model/corpus_eda.dump", "wb")
+#pickle.dump(corpus_eda, f)            # ファイルに保存
+#f.close()
 
+f = open('../model/corpus_eda.dump', 'rb')
+corpus_eda = pickle.load(f)
+f.close()
 
-corpus_eda = []
-for line in open('../corpus/full.eda'):
-    corpus_eda.append(line.strip())
-input_vector, corpus_vector, array = Dep2Feature.vectorizer(input_eda, corpus_eda, feature='word', vectorizer = 'count')  # vectorizer 
+#for line in open('../corpus/full.eda'):  # この形でもeda形式に落とせる
+#    corpus_eda.append(line.strip())
 
-tf = Dep2Feature.calculate_tf(array, 1)
-idf = Dep2Feature.calculate_idf(array)
+OBJ = Dep2Feature()  # インスタンス作成
+input_doc2vec = OBJ.vectorize_doc2vec(input_eda)
+corpus_doc2vec = OBJ.vectorize_doc2vec(corpus_eda)
+input_vector, corpus_vector = OBJ.vectorize(input_eda, corpus_eda, unigram = 1, dep_bigram = 1, dep_trigram = 1, vectorizer = 'tfidf')
 
-tf_idf = tf * idf
-input_vector, corpus_vector, array = Dep2Feature.vectorizer(input_eda, corpus_eda, feature='word', vectorizer = 'tfidf')  # vectorizer
+#idf = OBJ.calculate_idf()  # idfも引っ張ってこれる
+#tf = OBJ.calculate_tf(3)  # tfも持ってこれる
 
-
+Dep2Feature.sim_example(input_vector, corpus_vector, input_eda, corpus_eda)  # cos距離の実演
+Dep2Feature.sim_example(input_doc2vec, corpus_doc2vec, input_eda, corpus_eda)
 pdb.set_trace()
-#Dep2Feature.sim_example(input_vector, corpus_vector, input_eda, corpus_eda)  # cos距離の実演
-#Dep2Feature.calculate(input_list, sample_list, feature='word', number=5, vectorizer = 'tfidf') # tfidfモデル
-#Dep2Feature.calculate(input_list, sample_list, feature='word_dep', number=5, vectorizer = 'count')
-#Dep2Feature.calculate(input_list, sample_list, feature='word_dep', number=5, vectorizer = 'tfidf')
-#Dep2Feature.calculate(input_list, sample_list, feature='word_dep_uni', number=5, vectorizer = 'tfidf')
 
