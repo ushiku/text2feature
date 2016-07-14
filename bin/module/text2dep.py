6e41faf48f1160ec7e6830a6134b163c7816aa04
Atsushi Ushiku Local <atsushi@mgs06.(none)>
2016-07-14 15:13:45 +0900

    クラスか

--- bin/module/text2dep.py
+++ bin/module/text2dep.py
@@ -1,100 +1,139 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+class Text2dep:
 
-def text2dep(input_f, kytea_model=None, eda_model=""):
-    
-    import subprocess
-    import sys
-    import time
-    
-    #kytea,edaのインストールしているかの確認 
-    #以下の2つのコマンドだと終了ステータスが0ではないのでエラーが返ってくる
-    #終了ステータス1のときは成功としたが、環境によって異なるのかは分からん
-    try:
-        output_test = subprocess.check_output('kytea --help', shell=True)
-    except subprocess.CalledProcessError as e:
-        returncode =  e.returncode
-        if returncode == 1:
-            pass
+    #初期化とKyTea,EDAのコマンドがインストールされているか確認
+    def __init__(self):
+        import subprocess
+        import sys
+
+        #kytea,edaのインストールしているかの確認 
+        #以下の2つのコマンドだと終了ステータスが0ではないのでエラーが返ってくる
+        #終了ステータス1のときは成功としたが、環境によって異なるのかは分からん
+        try:
+            output_test = subprocess.check_output('kytea --help', shell=True)
+        except subprocess.CalledProcessError as e:
+            returncode =  e.returncode
+            if returncode == 1:
+                pass
+            else:
+                print ('"kytea --help"のコマンドが使えません')
+                print ('正しくインストールされているか確認してください')
+                print ('http://www.phontron.com/kytea/index-ja.html')
+                sys.exit(1)
+
+        try:
+            output_test = subprocess.check_output('eda', shell=True)
+        except subprocess.CalledProcessError as e:
+            returncode = e.returncode
+            if returncode == 1:
+                pass
+            else:
+                print ('"eda"のコマンドが使えません')
+                print ('正しくインストールされているか確認してください')
+                print ('http://www.ar.media.kyoto-u.ac.jp/tool/EDA/')
+
+
+    #KyTeaコマンド実行部分
+    def kytea(self, input_f, kytea_model=None, pipe_eda=False):        
+        import subprocess
+        import sys
+
+        #コマンドの定義、モデルを何にするかを決定
+        if kytea_model is None:
+            cmd_kytea = 'kytea'
         else:
-            print ('"kytea --help"のコマンドが使えません')
-            print ('正しくインストールされているか確認してください')
-            print ('http://www.phontron.com/kytea/index-ja.html')
-            sys.exit(1)
+            cmd_kytea = 'kytea -model ' + kytea_model
 
-    try:
-        output_test = subprocess.check_output('eda', shell=True)
-    except subprocess.CalledProcessError as e:
-        returncode = e.returncode
-        if returncode == 1:
-            pass
+        #複数ファイルをつなげる。ファイル終端にはEOFを追加
+        input = ""
+        for file_iter in input_f:
+            input = input + open(file_iter, 'r').read()
+            input = input + 'EOF\n'
+
+
+        #kytea実行部分
+        process_kytea = subprocess.Popen(cmd_kytea.strip().split(" "), stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+        output_kytea = process_kytea.communicate(input.encode('utf-8'))[0]
+
+        #False ならEOFごとに区切って、UTF-8にしてリスト型で返す
+        #Ture ならEDAに渡すためにそのまま返す
+        if pipe_eda == False:
+            #kyteaの出力を整形、EOFで区切ってlistに追加するために
+            #kyteaのEOFの出力を取得したり、それで分割したり、文字コード考慮したり
+            kytea_line_list = output_kytea.decode('utf-8').split("\n")
+            EOF_kytea = kytea_line_list[output_kytea.decode('utf-8').count('\n') - 1]
+
+            EOF_kytea = EOF_kytea + '\n'
+            output_kytea_list = output_kytea.split(EOF_kytea.encode('utf-8'))
+            for i in range(len(output_kytea_list)):
+                output_kytea_list[i] = output_kytea_list[i].decode('utf-8')
+
+            output_kytea_list.pop()
+            return output_kytea_list
+
         else:
-            print ('"eda"のコマンドが使えません')
-            print ('正しくインストールされているか確認してください')
-            print ('http://www.ar.media.kyoto-u.ac.jp/tool/EDA/')
-            
-    start = time.time()
+            print ('KyTea Done\n')
+            return output_kytea
 
-
-    #コマンドの定義、モデルを何にするかを決定
-    if kytea_model is None:
-        cmd_kytea = 'kytea'
-    else:
-        cmd_kytea = 'kytea -model ' + kytea_model
-    cmd_eda = 'eda -m ' + eda_model + '  -i kytea'
-
-
-    #複数ファイルをつなげる。ファイル終端にはEOFを追加
-    #ファイルの中身にEOFがあったら困る
-    input = ""
-    for file_iter in input_f:
-        input = input + open(file_iter, 'r').read()
-        input = input + 'EOF\n'
-
-
-    #kytea実行部分
-    process_kytea = subprocess.Popen(cmd_kytea.strip().split(" "), stdin=subprocess.PIPE, stdout=subprocess.PIPE)
-    output_kytea = process_kytea.communicate(input.encode('utf-8'))[0]
-    print ('kytea_done')
-    print(output_kytea.decode('utf-8'))
-
-    #eda実行部分
-    process_eda = subprocess.Popen(cmd_eda.strip().split(" "), stdin=subprocess.PIPE, stdout=subprocess.PIPE)
-    output_eda = process_eda.communicate(output_kytea)[0]
-
-    #EDAの出力を整形、EOFで区切ってlistに追加するために
-    #EDAのEOFの出力を取得したり、それで分割したり、文字コード考慮したり
-    eda_line_list = output_eda.decode('utf-8').split("\n")
-    EOF_eda = eda_line_list[output_eda.decode('utf-8').count('\n') - 2]
+    #EDAコマンド実行部分
+    def eda(self, input_kytea, eda_model='', pipe_kytea=False):
+        import subprocess
+        import sys
         
-    EOF_eda = EOF_eda + '\n'
-    output_eda_list = output_eda.split(EOF_eda.encode('utf-8'))
-    for i in range(len(output_eda_list) - 1):
-        output_eda_list[i] = output_eda_list[i].decode('utf-8')
+        #コマンドの定義、モデルを何にするかを決定
+        cmd_eda = 'eda -m ' + eda_model + '  -i kytea'
 
-    #かかった時間を表示、ここは不必要と言われるかも
-    elapsed_time = time.time() - start
-    print (("elapsed_time:{0}".format(elapsed_time)) + "[sec]")
-    
-    new_article = []
-    new_output = []
-    print(output_eda_list)
-    for article in output_eda_list:
-        if article == b'\n':
-            continue
-        sentences = article.split('ID=')
-        sentences.pop(0)
-        for sentence in sentences:
+        #eda実行部分
+        process_eda = subprocess.Popen(cmd_eda.strip().split(" "), stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+        if pipe_kytea == False:
+            input = ""
+            for file_iter in input_kytea:
+                input = input + open(file_iter, 'r').read()
+                input = input + 'EOF/名詞/いーおーえふ\n'
+            output_eda = process_eda.communicate(input.encode('utf-8'))[0]
+        else:
+            output_eda = process_eda.communicate(input_kytea)[0]
+
+        #EDAの出力を整形、EOFで区切ってlistに追加するために 
+        #EDAのEOFの出力を取得したり、それで分割したり、文字コード考慮したり
+        eda_line_list = output_eda.decode('utf-8').split("\n")
+        EOF_eda = eda_line_list[output_eda.decode('utf-8').count('\n') - 2]
+
+        EOF_eda = EOF_eda + '\n'
+        output_eda_list = output_eda.split(EOF_eda.encode('utf-8'))
+        for i in range(len(output_eda_list)):
+            output_eda_list[i] = output_eda_list[i].decode('utf-8')
+
+        output_eda_list.pop()
+        
+        new_article = []
+        new_output = []
+        print(output_eda_list)
+        for article in output_eda_list:
+            if article == b'\n':
+                continue
+            sentences = article.split('ID=')
+            sentences.pop(0)
+            for sentence in sentences:
             units = sentence.split('\n')
             units.pop(-1)
             units.pop(-1)
             try:
                 units.pop(0)
-                new_article.append(units)
+                        new_article.append(units)
             except:
                 pass
-        new_output.append(new_article)
-        new_article = []
+            new_output.append(new_article)
+            new_article = []
+        return new_output, output_eda_list
 
-    return new_output, output_eda_list
+
+
+        
+
+    # KyTeaの出力をEDAに渡しただけ
+    def t2f(self, input_f, kytea_model=None, eda_model=''):
+        
+        return self.eda(self.kytea(input_f, kytea_model, pipe_eda=True), eda_model, pipe_kytea=True)
