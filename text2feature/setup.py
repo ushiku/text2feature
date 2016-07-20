a840d795bc3e87bd690b777d52f1344e55cf8b45
Atsushi Ushiku Local <atsushi@mgs06.(none)>
2016-07-20 13:34:24 +0900

    setup.pyを作成_for_pip

---
+++ text2feature/setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+from setuptools import setup, find_packages
+from text2feature import __author__, __version__, __license__
+ 
+setup(
+        name             = 'text2feature',
+        version          = __version__,
+        description      = 'textからfeatureにする',
+        license          = __license__,
+        author           = __author__,
+        author_email     = '',
+        url              = '',
+        keywords         = '',
+        packages         = find_packages(),
+        install_requires = [],
+        )
