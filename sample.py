eda5810da90b5022591c0483755d40cee4b3ffc6
Atsushi Ushiku Local <atsushi@mgs06.(none)>
2016-07-27 15:53:49 +0900

    サンプル変更

--- sample.py
+++ sample.py
@@ -21,9 +21,17 @@ eda_file_path_list = ['corpus/sample.eda', 'corpus/sample.eda']
 input_eda = Text2dep.load_eda(eda_file_path_list)
 
 OBJ = Dep2Feature(input_eda, corpus_eda)  # インスタンス作成
-input_vector1, corpus_vector1 = OBJ.vectorize(unigram = 1, bigram = 0, trigram = 0, dep_bigram = 0, dep_trigram = 0, vectorizer = 'tfidf')  # Vecotrize
+input_vector1, corpus_vector1 = OBJ.vectorize(unigram = 1, bigram = 1, trigram = 1, dep_bigram = 0, dep_trigram = 0, vectorizer = 'tfidf')  # Vecotrize
 
 idf = OBJ.calculate_idf()  # idfも引っ張ってこれる
 tf = OBJ.calculate_tf(1)  # tfも持ってこれる
 
-OBJ.sim_example_jac(input_vector1, corpus_vector1, number = 5)  # cos距離の例を表示する
+sim_vector_cos = OBJ.sim_example_cos(input_vector1, corpus_vector1)
+sim_vector_dic = OBJ.sim_example_cos(input_vector1, corpus_vector1)
+sim_vector_sim = OBJ.sim_example_cos(input_vector1, corpus_vector1)
+sim_vector_jac = OBJ.sim_example_cos(input_vector1, corpus_vector1)
+
+OBJ.sim_print(OBJ.eda2unigram(input_eda), OBJ.eda2unigram(corpus_eda), sim_vector_cos)
+OBJ.sim_print(OBJ.eda2unigram(input_eda), OBJ.eda2unigram(corpus_eda), sim_vector_dic)
+OBJ.sim_print(OBJ.eda2unigram(input_eda), OBJ.eda2unigram(corpus_eda), sim_vector_sim)
+OBJ.sim_print(OBJ.eda2unigram(input_eda), OBJ.eda2unigram(corpus_eda), sim_vector_jac)
