ab7f85b67f09bb2466e80fcaa85d2a8ed9a412ae
ushiku <d460655046504@gmail.com>
2016-08-22 16:05:54 +0900

    サンプル改定

--- sample.py
+++ sample.py
@@ -1,37 +1,36 @@
 import sys
 import pdb
 import pickle
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_extraction.text import CountVectorizer
 from text2feature.dep2feature import Dep2Feature
 from text2feature.text2dep import Text2dep
 
-# ファイルの読み込み
-#input_list = ['corpus/input/1.txt', 'corpus/input/2.txt']
-#input_eda, input_eda_raw = Text2dep().t2f(input_list,  kytea_model='model/model.bin', eda_model='model/bccwj-20140727.etm')
-#f = open("model/eda.dump", "wb")
-#pickle.dump(input_eda, f)            # ファイルに保存
-#f.close()
+
+m = Text2dep()
+input_list = ['input1.txt', 'input2.txt']
+
+input_kytea = m.kytea(input_list, kytea_model='model/model.bin')
+input_eda1 = m.eda(input_kytea, eda_model='model/bccwj-20140727.etm')
+
+input_list = ['input3.txt']
+input_eda2 = m.t2f(input_list, kytea_model='model/model.bin', eda_model='model/bccwj-20140727.etm')
+
+input_eda3 = m.load_eda(['input4.eda'])
+
+input_eda4 = m.kytea2eda(input_kytea)
+
+D2F = Dep2Feature([input_eda1, input_eda2, input_eda3], unigram = 1, bigram = 1, vectorizer=CountVectorizer())  # 辞書作成
+
+vectors = D2F.vectorize([input_eda1, input_eda2])
+input_vector1 = vectors[0]
+input_vector2 = vectors[1]
 
 
-#かかり受けに時間がかかるので、読み込み用。計算用の大規模corpusの読み込み
-f = open('model/corpus_eda.dump', 'rb')
-corpus_eda = pickle.load(f)
-f.close()
+sim_matrix_cos = D2F.sim_example_cos(input_vector1, input_vector2)
+sim_matrix_dic = D2F.sim_example_dic(input_vector1, input_vector2)
+sim_matrix_sim = D2F.sim_example_sim(input_vector1, input_vector2)
+sim_matrix_jac = D2F.sim_example_jac(input_vector1, input_vector2)
 
-eda_file_path_list = ['corpus/sample.eda', 'corpus/sample.eda']
-input_eda = Text2dep.load_eda(eda_file_path_list)
-
-OBJ = Dep2Feature(input_eda, corpus_eda)  # インスタンス作成
-input_vector1, corpus_vector1 = OBJ.vectorize(unigram = 1, bigram = 1, trigram = 1, dep_bigram = 0, dep_trigram = 0, vectorizer = 'tfidf')  # Vecotrize
-
-idf = OBJ.calculate_idf()  # idfも引っ張ってこれる
-tf = OBJ.calculate_tf(1)  # tfも持ってこれる
-
-sim_vector_cos = OBJ.sim_example_cos(input_vector1, corpus_vector1)
-sim_vector_dic = OBJ.sim_example_cos(input_vector1, corpus_vector1)
-sim_vector_sim = OBJ.sim_example_cos(input_vector1, corpus_vector1)
-sim_vector_jac = OBJ.sim_example_cos(input_vector1, corpus_vector1)
-
-OBJ.sim_print(OBJ.eda2unigram(input_eda), OBJ.eda2unigram(corpus_eda), sim_vector_cos)
-OBJ.sim_print(OBJ.eda2unigram(input_eda), OBJ.eda2unigram(corpus_eda), sim_vector_dic)
-OBJ.sim_print(OBJ.eda2unigram(input_eda), OBJ.eda2unigram(corpus_eda), sim_vector_sim)
-OBJ.sim_print(OBJ.eda2unigram(input_eda), OBJ.eda2unigram(corpus_eda), sim_vector_jac)
+print('cos類似度')
+D2F.sim_print(D2F.eda2unigram(input_eda1), D2F.eda2unigram(input_eda2), sim_matrix_cos, number = 2)
